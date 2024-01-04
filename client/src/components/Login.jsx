e401f2d9411f6e1f6f9eed133faf4de569deae4d
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2024-01-04 08:21:09 +0000

    login page frontend task done

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,30 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
+const signUpInitialValues = {
+  name: "",
+  username: "",
+  password: "",
+};
+
 const Login = () => {
   const imageUrl =
     "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
 
   const [account, toggleAccount] = useState("login");
+  const [signUp, setSignUp] = useState(signUpInitialValues);
+  console.log(signUp);
 
   const toggleSignUp = () => {
     account === "signup" ? toggleAccount("login") : toggleAccount("signup");
   };
+
+  const onInputChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    // setSignUp(e.target.name, e.target.value) //is tarike se hmare values override ho jaenge
+    setSignUp({ ...signUp, [e.target.name]: e.target.value });  //yahan pe old values ...signup me hoga and e.target.name ab as a key kaam krega kyuki hme values pass karwani hai object me 
+  };
+
   return (
     <>
       <Box //box is a replacement of div
@@ -81,18 +96,24 @@ const Login = () => {
               label="Enter name"
               variant="standard"
               style={{ marginTop: "20px" }}
+              name="name"
+              onChange={(e) => onInputChange(e)}
             />
             <TextField
               id="standard-basic"
               label="Enter username"
               variant="standard"
               style={{ marginTop: "20px" }}
+              name="username"
+              onChange={(e) => onInputChange(e)}
             />
             <TextField
               id="standard-basic"
               label="Enter password"
               variant="standard"
+              name="password"
               style={{ marginTop: "20px" }}
+              onChange={(e) => onInputChange(e)}
             />
             <Button
               variant="contained"
